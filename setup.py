0ae15838d454cc438c972a68ab9216b2f367340a
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-12-05 01:12:48 +0000

    Setup.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='flat',
+    author='Lucian Branescu Mihaila',
+    version='0.1',
+    license='MIT',
+    description='Flattening and unflattening utils.',
+    py_modules='flat',
+)
