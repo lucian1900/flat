328feb64b474e5cbd8d44d16ff4ee172e64aaadc
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-12-11 23:03:29 +0000

    Add readme.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open('README') as f:
+    long_description = f.read()
+
 setup(
     name='flat',
     author='Lucian Branescu Mihaila',
     version='0.1',
     license='MIT',
     description='Flattening and unflattening utils.',
+    long_description=long_description,
     py_modules='flat',
 )
